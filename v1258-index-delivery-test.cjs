66bdf868e1092fc54a9bf1ad7931574b0ffa80c8
dennis-glaser-coder <dennis-glaser@gmx.de>
2026-08-13 05:36:03 +0200

    remove blocked V12.58 delivery assertion

--- v1258-index-delivery-test.cjs
+++
@@ -1,5 +0,0 @@
-const fs=require('fs');
-const html=fs.readFileSync('./index.html','utf8');
-const ok=html.includes('<script src="./noreyo-v1258.js?build=1258"></script>')&&html.includes("const SRC='./noreyo-bootstrap-v1257.js?build=1257'");
-console.log(ok?'PASS V12.58 touch layer is explicitly delivered alongside verified V12.57 bootstrap':'FAIL V12.58 index delivery');
-process.exit(ok?0:1);
